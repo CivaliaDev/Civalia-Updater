11c6ec190ea2534354601dd1bd0aefe1d505c17a
Nokaji <oximas.yt@gmail.com>
2023-07-08 21:09:03 +0200

    update

---
+++ build.js
@@ -0,0 +1,78 @@
+const builder = require('electron-builder');
+const Platform = builder.Platform;
+
+function getCurrentPlatform() {
+    switch(process.platform) {
+        case 'win32': {
+            return Platform.WINDOWS;
+        }
+        case 'darwin': {
+            return Platform.MAC;
+        }
+        case 'linux': {
+            return Platform.linux;
+        }
+        default: {
+            console.error('Cannot resolve current platform!');
+            return undefined;
+        }
+    }
+}
+
+builder.build( {
+    targets: (process.argv[2] != null && Platform[process.argv[2]] != null ? Platform[process.argv[2]] : getCurrentPlatform()).createTarget(),
+    config: {
+        appId: 'civaliaupdate',
+        productName: 'Civalia Update',
+        artifactName: '${productName}.${ext}',
+        copyright: 'Copyright © 2019 Uranium',
+        directories: {
+            // buildResources: 'assets',
+            output: 'dist'
+        },
+        publish: [{
+            provider: "github",
+            owner: "CivaliaDev",
+            repo: "Civalia-Updater"
+        }],
+        win: {
+            target: [
+                {
+                    target: 'nsis',
+                    arch: 'x64'
+                }
+            ]
+            // icon: 'app/assets/images/icons/icon.ico'
+        },
+        nsis: {
+            oneClick: false,
+            perMachine: true,
+            allowElevation: true,
+            // installerIcon: 'app/assets/images/icons/icon.ico',
+            // uninstallerIcon: 'app/assets/images/icons//icon.ico',
+            allowToChangeInstallationDirectory: true
+        },
+        mac: {
+            target: 'dmg',
+            category: 'public.app-category.games',
+            icon: 'app/assets/images/icons/icon.icns'
+        },
+        linux: {
+            target: 'AppImage',
+            maintainer: 'Nokaji',
+            vendor: 'Uranium',
+            synopsis: 'Uranium Launcher',
+            description: 'Uranium Launcher',
+            category: 'Game'
+        },
+        compression: 'maximum',
+        files: [
+            '!{dist,.gitignore,.vscode,docs,dev-app-update.yml,.travis.yml,.nvmrc,.eslintrc.json,build.js}'
+        ],
+        asar: true
+    }
+}).then(() => {
+    console.log('Build complete!');
+}).catch(err => {
+    console.error('Error during build!', err);
+})
